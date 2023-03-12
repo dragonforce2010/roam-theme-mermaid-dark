6e4afa29f628d62f16c36c33c15cb4097cd5efe4
dragonforce2010 <coding.monkey.github@gmail.com>
2023-03-13 02:30:31 +0800

    new theme

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+
+
+function onload() {
+  document.body.className = 'mermaid-dark'
+}
+
+function onunload() {
+
+}
+
+export default {
+  onload: onload,
+  onunload: onunload,
+}
